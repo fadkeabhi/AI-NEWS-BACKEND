4971b0196edb13657abc6357730af81005534194
Sid-80 <siddharthbramhecha@gmail.com>
2024-07-25 21:27:11 +0530

    fix: error

--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -25,7 +25,7 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
     next();
   } catch (err) {
     console.log(err);
-    return res.status(500).json({message:"Error occured!"})
+    return res.status(500).json({message:`Error occured! ${err}`})
   }
 });
 
